6abc649326c9f3d755d4a034d0f14b8ea4ac8e03
Alaa997 <alaa.tarakji997@gmail.com>
2023-11-30 18:20:34 +0100

    Integrated Global state management into App

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import HomePage from "./components/Pages/HomePage";
 import About from "./components/Pages/About";
 import TodoPage from "./components/Pages/TodoPage";
 import { Box } from "@mui/material";
+import { Global } from "@emotion/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Box sx={{ display: "flex" }}>
-        <SideBar />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/todo" element={<TodoPage />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Box>
-      </Box>
-    </BrowserRouter>
+    <Global
+      Root={() => (
+        <BrowserRouter>
+          <Box sx={{ display: "flex" }}>
+            <SideBar />
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/todo" element={<TodoPage />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+            </Box>
+          </Box>
+        </BrowserRouter>
+      )}
+    />
   );
 }
 
