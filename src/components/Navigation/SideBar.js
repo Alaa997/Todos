2af628b9e991cd479eb787647424133cb3da2819
Alaa997 <alaa.tarakji997@gmail.com>
2023-11-30 18:03:07 +0100

    Updated Sidebar for responsive design using Material-UI

--- src/components/Navigation/SideBar.js
+++ src/components/Navigation/SideBar.js
@@ -1,26 +1,66 @@
-import { Drawer, List, ListItem, ListItemText } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawerContent = (
+    <List>
+      {["Home", "Todo", "About"].map((text) => (
+        <ListItem
+          button
+          key={text}
+          component={Link}
+          to={text === "Home" ? "/" : `/${text.toLowerCase()}`}
+        >
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  );
+
   return (
-    <Drawer variant="permanent" anchor="left">
-      <List>
-        {["Home", "Todo", "About"].map((text, index) => (
-          <ListItem
-            key={text}
-            component={Link}
-            to={
-              text === "Home"
-                ? "/"
-                : `/${text.replace(/\s+/g, "").toLowerCase()}`
-            }
-          >
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
+    <nav>
+      {isMobile && (
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
+      )}
+      <Drawer
+        variant={isMobile ? "temporary" : "permanent"}
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": { width: 240, boxSizing: "border-box" },
+        }}
+      >
+        {drawerContent}
+      </Drawer>
+    </nav>
   );
 };
 
