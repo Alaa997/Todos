86e55a4489a281eb17061fd6f27313c2c0a9c683
Alaa997 <alaa.tarakji997@gmail.com>
2023-11-30 18:01:23 +0100

    update the structure using "Box", to the left a menu, to the right the content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-
 import SideBar from "./components/Navigation/SideBar";
 import HomePage from "./components/Pages/HomePage";
 import About from "./components/Pages/About";
 import TodoPage from "./components/Pages/TodoPage";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <BrowserRouter>
-      <SideBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/todo" element={<TodoPage />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <Box sx={{ display: "flex" }}>
+        <SideBar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/todo" element={<TodoPage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Box>
+      </Box>
     </BrowserRouter>
   );
 }
